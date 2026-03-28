204dc732b135b1fc06427cd0b3c45cc1cdabd328
Mart Sebastian <sebi.mart04@gmail.com>
2026-03-28 12:32:16 +0000

    commit primul la fii mar. 28__a

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ IMAGE_KEY   = "${image_key}"
 
 s3_client = boto3.client('s3', region_name=REGION)
 dynamodb  = boto3.resource('dynamodb', region_name=REGION)
-table     = dynamodb.Table(TABLE_NAME)
+table     = dynamodb.Table(TABLE_NAME)#da
 
 def get_metadata(path):
     try:
